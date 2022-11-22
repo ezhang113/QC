ac43dc5f12d26a49e168210c767068e496380eff
ezhang113 <ezhang113@gmail.com>
2022-11-22 11:35:41 -0800

    Update blastresults_piechart.py

--- bash scripts/blastresults_piechart.py
+++ bash scripts/blastresults_piechart.py
@@ -9,12 +9,43 @@ from xml.dom import minidom
 
 #handles command line input parameter
 import sys
+import os
 
 larp6_file = sys.argv[1]
 
+#ncbi query functions
+def esearch(term, db='gds'):
+    """
+    Queries NCBI using the esearch utility. GEO ('gds') database is used as default for search term.
+    """
+    url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db={db}&term={term}&retmax=5000&usehistory=y'
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+
+def get_esummary(esearch_string, db='gds'):
+    """
+    Parses a http response in XML format to obtain the webenv and querykey tokens.
+    Uses NCBI eutils to transform these tokens into web summaries of GEO (db='gds') datasets.
+    """
+    xmldoc = minidom.parseString(esearch_string)
+    try:
+        webenv = xmldoc.getElementsByTagName('WebEnv')[0].firstChild.data
+        querykey = xmldoc.getElementsByTagName('QueryKey')[0].firstChild.data
+        host = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi'
+        params = f'?db={db}&version=2.0&query_key={querykey}&WebEnv={webenv}'
+        url = host + params
+        response = urllib.request.urlopen(url)
+        return response.read()
+    except IndexError as e:
+        print(f"Unparsable publication string ({e}, search={esearch_string}")
+        return ""
+
+#matplotlib to build the piechart
 fig, ax = plt.subplots()
 
 df = pd.read_csv(larp6_file, sep='\t')
+num_seqs = df.size
 df.columns = ['qseqid','sseqid','pident','length','mismatch','gapopen','qstart','qend','sstart','send','evalue','bitscore']
 
 #use groupby to find frequency of each sseq, stored in new dataframe
@@ -24,41 +55,68 @@ sseq_count_series = df['sseqid'].value_counts()
 to_remove = []
 other_count = 0
 
+print(num_seqs)
+
+#taking 1% of number of sequences
 for index,values in sseq_count_series.iteritems():
-        if(values < 50000):
+        if(values < (0.01)*(num_seqs/5)):
                 to_remove.append(index)
                 other_count += values
 
-print(sseq_count_series.size)
 
 #remove these elements from series
 sseq_count_series2 = sseq_count_series.drop(to_remove)
-print(sseq_count_series2.size)
 
+sseq_name_list = []
+sseqid = []
 #replace index sseqids with ncbi name
-for index in sseq_count_series2.iteritems():
-        sseq_count_series.rename(index={index:#call ncbi query function})
+for index,values in sseq_count_series2.iteritems():
+        sseqid.append(index)
+        term = str(index)
+        esearch_string = esearch(term=term, db='nucleotide')
+        result = get_esummary(esearch_string=esearch_string, db='nucleotide')
+        sseq_count_series = df['sseqid'].value_counts()
+        result = xmltodict.parse(result)
+        sseq_name = result['eSummaryResult']['DocumentSummarySet']['DocumentSummary']['Title']
+        sseq_name_list.append(sseq_name)
+        #sseq_count_series2.rename(index={index:sseq_name})
 
-print(sseq_count_series)
+#replace the sseqids with ncbi query names
+replacements = {sseqid:sseq_name_list for sseqid, sseq_name_list in zip(sseqid, sseq_name_list)}
+sseq_count_series3 = sseq_count_series2.rename(replacements)
+#sseq_count_series2 = sseq_count_series2.rename(index=dict(zip(sseq_name_list,sseqid)))
 
 #generate new pandas series with new element to concatenate with old series
-#d = {'Other':other_count}
-#ser = pd.Series(data=d; index=['Other']
+d = {'Other':other_count}
+ser = pd.Series(data=d, index=['Other'])
 
 #append new element
-#sseq_count = sseq_count_series2.append(ser)
+sseq_count = sseq_count_series3.append(ser)
+#print(sseq_count)
 
 #value_counts returns a pandas series so convert to a data frame
-sseq_count_df = pd.DataFrame({'sseqid':sseq_count_series.index, 'count':sseq_count_series.values})
+sseq_count_df = pd.DataFrame({'sseqid':sseq_count.index, 'count':sseq_count.values})
+print(sseq_count_df)
 
-#count = sseq_count_df['count']
-#sseq = sseq_count_df['sseqid']
+count = sseq_count_df['count']
+sseq = sseq_count_df['sseqid']
 
-#ax.pie(count, labels = sseq, colors=None, radius=3, center=(4, 4),
-#	wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True)
+ax.pie(count, labels = sseq, colors=None,autopct='%1.1f%%',startangle=45,
+wedgeprops={"linewidth": 1, "edgecolor": "white"})
 
 
-#plt.title('sseq counts summary LARP6')
-#plt.show(block=True)
-#plt.savefig("sseq_counts1")
+plt.title('unmapped sequences summary LARP6')
+plt.show(block=True)
+plt.savefig(filename[-1]+".png",format='png',bbox_inches='tight')
+
+
+
+
+
+
+
+
+
+
+
 
